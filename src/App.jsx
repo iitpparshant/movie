d682504907c1e822da9b7358e8b764db9be1ebbb
iitpparshant <parshantverma27.2003@gmail.com>
2024-01-20 18:08:42 +0530

    errot fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { getApiConfiguration,getGenres } from "./store/homeSlice";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Home from "./pages/home/Home";
-import PageNotFound from "./pages/404/pageNotFound";
+import PageNotFound from "./pages/404/PageNotFound";
 import Explore from "./pages/explore/Explore";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
